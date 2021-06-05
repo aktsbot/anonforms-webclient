0173780b939f7723be204f4d013033767789e15c
akts <a@aktsbot.in>
2021-06-05 11:34:07 +0530

    added dashboard template

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // all components
 import Auth from "./components/Auth";
 import ViewForm from "./components/ViewForm";
+import Dashboard from "./components/Dashboard";
 import NotFound from "./components/NotFound";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/auth" exact component={Auth} />
+        <Route path="/dashboard" exact component={Dashboard} />
         <Route path="/:form_uri" exact component={ViewForm} />
         <Route component={NotFound} />
       </Switch>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Page from "./Page";
+
+function Dashboard() {
+  // TODO: add state and bind view
+  return (
+    <Page title="Your dashboard">
+      <h1 className="head-underline">Dashboard</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title + creation date YYYY/MM/DD</th>
+            <th>Link</th>
+            <th>Responses</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              <a href="/">IPHACON Registration form</a> -{" "}
+              <small>2021/02/09</small>
+            </td>
+            <td>
+              <a href="/">https://foo.com/ha123</a>
+            </td>
+            <td>
+              <a href="/">23</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </Page>
+  );
+}
+
+export default Dashboard;
