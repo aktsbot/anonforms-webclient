4ffd2f622701b437e114be916a3d4cfd46fac6c2
akts <a@aktsbot.in>
2021-06-13 12:57:38 +0530

    added index page template

--- src/components/Index.js
+++ src/components/Index.js
@@ -11,10 +11,58 @@ function Index() {
     <Redirect to="/dashboard" />
   ) : (
     <Page title="Build and share forms anonymously">
-      <h1>Anonforms &mdash; Free &amp; anonymous form builder</h1>
-      <p>
-        <Link to="/auth">I'm ready to build a form</Link>
-      </p>
+      <div className="text-center">
+        <h1 className="head-underline">Anonforms</h1>
+
+        <div className="row">
+          <div className="col c4">
+            <h2>Anonymous</h2>
+            <ul className="text-left">
+              <li>
+                Authorize with any email-id and get started with making forms.
+              </li>
+              <li>
+                Feel free to use a disposable-mail services like{" "}
+                <a href="//mailinator.com">mailinator.com</a> or{" "}
+                <a href="//yopmail.com">yopmail</a>.
+              </li>
+              <li>
+                Email IDs are <strong>hashed</strong> before being added to the
+                system.
+              </li>
+            </ul>
+          </div>
+          <div className="col c4">
+            <h2>Self-hosted</h2>
+            <ul className="text-left">
+              <li>Power to the users. Run your own instance of anonforms!</li>
+              <li>
+                Check out the{" "}
+                <a href="//github.com/aktsbot/anonforms-server/documentation.md">
+                  installation guide
+                </a>
+                .
+              </li>
+            </ul>
+          </div>
+          <div className="col c4">
+            <h2>Free and open source</h2>
+            <ul className="text-left">
+              <li>Built with React, NodeJS and MongoDB.</li>
+              <li>
+                Check out the source code on{" "}
+                <a href="//github.com/aktsbot/anonforms-server">github.com</a>.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row text-center m-t-sm m-b-sm">
+          <Link to="/auth" className="btn btn-b smooth">
+            Get started
+          </Link>
+        </div>
+      </div>
     </Page>
   );
 }
